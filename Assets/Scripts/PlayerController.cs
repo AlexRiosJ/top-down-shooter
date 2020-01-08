103118904fffc6ec594d4d59f2e9dbc7dc1abd14
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-01-08 11:41:02 -0600

    Creating project and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Vector3 velocity;
+    Rigidbody rigidbody;
+
+    void Start () {
+        rigidbody = GetComponent<Rigidbody> ();
+    }
+
+    public void Move (Vector3 _velocity) {
+        velocity = _velocity;
+    }
+
+    public void LookAt (Vector3 lookPoint) {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt (heightCorrectedPoint);
+    }
+
+    public void FixedUpdate () {
+        rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
