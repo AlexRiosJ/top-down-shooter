112110fe94fdc203cadb11ca673d57b0de2f295c
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-01-11 13:38:43 -0600

    Addign new scene for map generator (tile map)

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapEditor : Editor {
+
+    public override void OnInspectorGUI () {
+        base.OnInspectorGUI ();
+
+        MapGenerator map = target as MapGenerator;
+        map.GenerateMap ();
+    }
+
+}
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour {
-    
+
     public Transform muzzle;
     public Projectile projectile;
     public float msBetweenShots = 100;
@@ -11,11 +11,11 @@ public class Gun : MonoBehaviour {
 
     float nextShotTime;
 
-    public void Shoot() {
-        if(Time.time > nextShotTime) {
+    public void Shoot () {
+        if (Time.time > nextShotTime) {
             nextShotTime = Time.time + msBetweenShots / 1000;
-            Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
-            newProjectile.SetSpeed(muzzleVelocity);
+            Projectile newProjectile = Instantiate (projectile, muzzle.position, muzzle.rotation) as Projectile;
+            newProjectile.SetSpeed (muzzleVelocity);
         }
     }
 }
