c75666237adfa24cea0af9dbb712056949d227f8
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-01-11 14:13:29 -0600

    Adding a placement for obstacles in map randomly

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour {
 
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
 
     [Range (0, 1)]
     public float outlinePercent;
+
+    public int seed = 10;
+
+    List<Coord> allTileCoords;
+    Queue<Coord> shuffledTileCoords;
 
     void Start () {
         GenerateMap ();
     }
 
     public void GenerateMap () {
+
+        allTileCoords = new List<Coord> ();
 
         string holderName = "Generated Map";
         if (transform.Find (holderName)) {
@@ -26,11 +34,44 @@ public class MapGenerator : MonoBehaviour {
 
         for (int x = 0; x < mapSize.x; x++) {
             for (int y = 0; y < mapSize.y; y++) {
-                Vector3 tilePosition = new Vector3 (-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+
+                allTileCoords.Add (new Coord (x, y));
+
+                Vector3 tilePosition = CoordToPosition (x, y);
                 Transform newTile = Instantiate (tilePrefab, tilePosition, Quaternion.Euler (Vector3.right * 90)) as Transform;
                 newTile.localScale = Vector3.one * (1 - outlinePercent);
                 newTile.parent = mapHolder;
             }
+        }
+
+        shuffledTileCoords = new Queue<Coord> (Utility.ShuffleArray (allTileCoords.ToArray (), seed));
+
+        int obstacleCount = 10;
+        for (int i = 0; i < obstacleCount; i++) {
+            Coord randomCoord = GetRandomCoord ();
+            Vector3 obstaclePosition = CoordToPosition (randomCoord.x, randomCoord.y);
+            Transform newObstacle = Instantiate (obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity) as Transform;
+            newObstacle.parent = mapHolder;
+        }
+    }
+
+    Vector3 CoordToPosition (int x, int y) {
+        return new Vector3 (-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+    }
+
+    public Coord GetRandomCoord () {
+        Coord randomCoord = shuffledTileCoords.Dequeue ();
+        shuffledTileCoords.Enqueue (randomCoord);
+        return randomCoord;
+    }
+
+    public struct Coord {
+        public int x;
+        public int y;
+
+        public Coord (int _x, int _y) {
+            x = _x;
+            y = _y;
         }
     }
 }
