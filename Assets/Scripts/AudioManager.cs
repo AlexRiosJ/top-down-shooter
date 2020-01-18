466837bcd44a4862b70984969b7754ddeb1d87bb
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-01-18 01:03:11 -0600

    Addign menu ui with respective functions

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,9 +6,9 @@ public class AudioManager : MonoBehaviour {
 
     public enum AudioChannel { Master, Sfx, Music }
 
-    float masterVolumePercent = 0.2f;
-    float sfxVolumePercent = 1;
-    float musicVolumePercent = 1;
+    public float masterVolumePercent { get; private set; }
+    public float sfxVolumePercent { get; private set; }
+    public float musicVolumePercent { get; private set; }
 
     AudioSource sfx2DSource;
     AudioSource[] musicSources;
@@ -40,11 +40,13 @@ public class AudioManager : MonoBehaviour {
             newSfx2DSource.transform.parent = transform;
 
             audioListener = FindObjectOfType<AudioListener> ().transform;
-            playerT = FindObjectOfType<Player> ().transform;
+            if (FindObjectOfType<Player> () != null) {
+                playerT = FindObjectOfType<Player> ().transform;
+            }
 
-            masterVolumePercent = PlayerPrefs.GetFloat ("master vol", masterVolumePercent);
-            sfxVolumePercent = PlayerPrefs.GetFloat ("sfx vol", sfxVolumePercent);
-            musicVolumePercent = PlayerPrefs.GetFloat ("music vol", musicVolumePercent);
+            masterVolumePercent = PlayerPrefs.GetFloat ("master vol", 1);
+            sfxVolumePercent = PlayerPrefs.GetFloat ("sfx vol", 1);
+            musicVolumePercent = PlayerPrefs.GetFloat ("music vol", 1);
         }
     }
 
@@ -73,6 +75,7 @@ public class AudioManager : MonoBehaviour {
         PlayerPrefs.SetFloat ("master vol", masterVolumePercent);
         PlayerPrefs.SetFloat ("sfx vol", sfxVolumePercent);
         PlayerPrefs.SetFloat ("music vol", musicVolumePercent);
+        PlayerPrefs.Save ();
     }
 
     public void PlayMusic (AudioClip clip, float fadeDuration = 1) {
